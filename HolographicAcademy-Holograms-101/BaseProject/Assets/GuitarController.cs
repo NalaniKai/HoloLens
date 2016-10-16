8474a00ac093e32d5015bfd553049c3eb089dc45
Olga Kazakova <kazako@cs.washington.edu>
2016-10-16 15:35:24 -0700

    final

---
+++ HolographicAcademy-Holograms-101/BaseProject/Assets/GuitarController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuitarController : MonoBehaviour {
+    GameObject myWoman;
+    // Use this for initialization
+    void Start () {
+        myWoman = GameObject.Find("Charakter_Woman");
+    }
+    void OnSelect()
+    {
+        foreach (Renderer r in myWoman.transform.GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = true;
+        }
+        AudioSource sound = myWoman.GetComponent<WomanController>().getsound();
+        sound.Play();
+        Invoke("GoodBye", 24);
+
+
+    }
+    void GoodBye()
+    {
+        foreach (Renderer r in myWoman.transform.GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = false;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ HolographicAcademy-Holograms-101/BaseProject/Assets/WomanController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WomanController : MonoBehaviour {
+    public AudioClip womanStory;
+    public AudioSource womanSource;
+    // Use this for initialization
+    void Start () {
+        foreach (Renderer r in gameObject.transform.GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = false;
+        }
+        womanSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public AudioSource getsound()
+    {
+        return womanSource;
+    }
+}
